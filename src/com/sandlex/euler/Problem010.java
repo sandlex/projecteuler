ef3447caca59a914eab9affcb52bf8253c4cb1e7
Alexey Peskov <aspeskov@gmail.com>
2016-03-08 22:31:20 +0100

    Problem 10 not done

---
+++ src/com/sandlex/euler/Problem010.java
@@ -0,0 +1,27 @@
+package com.sandlex.euler;
+
+/**
+ * author: Alexey Peskov
+ */
+public class Problem010 {
+
+    public static void main(String[] args) {
+        int num = 1, sum = 0;
+        while (num < 2000000) {
+            if (isPrime(++num)) {
+                sum += num;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private static boolean isPrime(int value) {
+        for (int i = 2; i <= Math.sqrt(value); i++) {
+            if (value % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
